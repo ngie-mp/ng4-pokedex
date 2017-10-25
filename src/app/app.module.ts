e4196ac360e74182d04dd758bd682de31d06a8b7
admin <admin@iMac3.local>
2017-10-25 14:18:53 +0200

    Style modifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { PokeapiService } from './services/pokeapi.service';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ErrorpageComponent } from './components/errorpage/errorpage.component';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
+  poketrainer = "Trainer name";
 
   constructor() { }
 
