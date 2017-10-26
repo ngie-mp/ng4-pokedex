9587f46fac48f460af23f0600ae99813c97d5620
ngie-mp <angeles.morap@gmail.com>
2017-10-26 15:10:37 +0200

    Pokemons listing

--- src/app/components/poke-list/poke-list.component.ts
+++ src/app/components/poke-list/poke-list.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { PokeapiService } from '../../services/pokeapi.service';
+import { PokeapiService, PokemonParams } from '../../services/pokeapi.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
-  providers: [PokeapiService]
 })
 
 export class PokeListComponent implements OnInit {
 
-  pokemonsList: {
-    name?: string,
-    sprites?: {}
-  };
+  pokemonsList = null;
 
   constructor(private pokemons:PokeapiService) { }
 
   ngOnInit() {
-    this.pokemons.getPokemonById(1)
+    this.pokemons.getPokemons()
       .then(data => {
         this.pokemonsList = data;
+        console.log(this.pokemonsList);
       })
       .catch(function(err){
         console.log(err);
--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-export interface PokemonRules {
-    id:number;
-    name:string;
-    image:string;
-    base_experience:number;
-    height:number;
-}
 
+// Hoping this works one day
+export interface PokemonParams {
+    id?: number;
+    name?: string;
+    image?: string;
+    base_experience?: number;
+    height?: number;
+    results?: Array<any>;
+}
 
 @Injectable()
 export class PokeapiService {
 
   constructor(private http:Http) { }
 
-  getPokemonById(id: Number) {
+  getPokemons() {
     return new Promise((resolve,reject) => {
-      this.http.get("https://pokeapi.co/api/v2/pokemon/"+id)
-        .toPromise()
-        .then(data => {
-          resolve(data);
-        }, err => {
-          reject(err);
+      this.http.get("https://pokeapi.co/api/v2/pokemon/")
+        .subscribe(data => {
+          resolve(data.json());
         })
     });
   }
